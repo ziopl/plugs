32b70433f81fa93bb597f292659e8a34fbd8db37
ziv <polackziv@gmail.com>
2020-11-03 12:05:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+![xKylee - CI (Push)](https://github.com/xKylee/plugins-source/workflows/xKylee%20-%20CI%20(Push)/badge.svg?branch=master)
+
+# xKylee Plugins (Source Code) Repository.
+
+This is a collection of un-official plugins, This repository is not affiliated with OpenOSRS, Runelite or Jagex in any way.
+
+# Discord
+https://discord.gg/ARcwsJr
---
+++ build.gradle.kts
@@ -0,0 +1,132 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    checkstyle
+}
+
+apply<BootstrapPlugin>()
+apply<VersionPlugin>()
+
+allprojects {
+    group = "com.openosrs"
+    version = ProjectVersions.rlVersion
+    apply<MavenPublishPlugin>()
+}
+
+subprojects {
+    group = "com.openosrs.externals"
+
+    project.extra["PluginProvider"] = "k"
+    project.extra["ProjectUrl"] = "https://discord.gg/ARcwsJr"
+    project.extra["PluginLicense"] = "3-Clause BSD License"
+
+    repositories {
+         jcenter {
+            content {
+                excludeGroupByRegex("com\\.openosrs.*")
+                excludeGroupByRegex("com\\.runelite.*")
+            }
+        }
+
+        exclusiveContent {
+            forRepository {
+                 maven {
+                    url = uri("https://repo.runelite.net")
+                }
+            }
+            filter {
+                includeModule("net.runelite", "discord")
+            }
+        }
+
+        exclusiveContent {
+            forRepository {
+                maven {
+                    url = uri("https://raw.githubusercontent.com/open-osrs/hosting/master")
+                }
+            }
+            filter {
+                includeModule("com.openosrs.rxrelay3", "rxrelay")
+            }
+        }
+
+        exclusiveContent {
+            forRepository {
+                maven {
+                    url = uri("https://raw.githubusercontent.com/open-osrs/hosting/master")
+                }
+                mavenLocal()
+            }
+            filter {
+                includeGroupByRegex("com\\.openosrs.*")
+            }
+        }
+    }
+
+    apply<JavaPlugin>()
+    apply(plugin = "checkstyle")
+
+    checkstyle {
+        maxWarnings = 0
+        toolVersion = "8.25"
+        isShowViolations = true
+        isIgnoreFailures = false
+    }
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                url = uri("$buildDir/repo")
+            }
+        }
+        publications {
+            register("mavenJava", MavenPublication::class) {
+                from(components["java"])
+            }
+        }
+    }
+
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    tasks {
+        withType<JavaCompile> {
+            options.encoding = "UTF-8"
+        }
+
+        withType<Jar> {
+            doLast {
+                copy {
+                    from("./build/libs/")
+                    into("../release/")
+                }
+            }
+        }
+
+        withType<AbstractArchiveTask> {
+            isPreserveFileTimestamps = false
+            isReproducibleFileOrder = true
+            dirMode = 493
+            fileMode = 420
+        }
+
+        withType<Checkstyle> {
+            group = "verification"
+
+            exclude("**/ScriptVarType.java")
+            exclude("**/LayoutSolver.java")
+            exclude("**/RoomType.java")
+        }
+
+        register<Copy>("copyDeps") {
+            into("./build/deps/")
+            from(configurations["runtimeClasspath"])
+        }
+    }
+}
